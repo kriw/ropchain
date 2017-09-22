5ebae79cfa216aa4f787f4456e9c68ad8616b391
kriw <kotarou777775@gmail.com>
2017-09-22 15:09:25 +0900

    delete --unique in ropgadget.sh

--- ropchain/frontend/ropgadget.sh
+++ ropchain/frontend/ropgadget.sh
@@ -4,7 +4,7 @@ dir="./.cache/ropchain/"
 [[ -f $dir ]] || mkdir -p $dir
 if [ ! -e $save ]
 then
-    rp++ --file=$file --rop=1 --unique |
+    rp++ --file=$file --rop=1 |
     grep '0x' |
     sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" |
     sed 's/(.* found)//' > $dir$save

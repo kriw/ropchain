2f46600fd6028f8500db5d1ae83183cba389d71a
kriw <kotarou777775@gmail.com>
2017-10-04 19:35:28 +0900

    upload latest package to PyPI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ class CustomInstallCommand(install):
  
 setup(
         name             = 'ropchain',
-        version          = '0.1.3',
+        version          = '0.1.4',
         description      = 'ROPChain generator',
         license          = 'GPL3.0',
         author           = 'kriw',

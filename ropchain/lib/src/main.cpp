3b5341385198eb5331e3e09ce04989b92d8c552b
kriw <kotarou777775@gmail.com>
2018-01-17 12:54:24 +0100

    update arg parser

--- ropchain/lib/src/main.cpp
+++ ropchain/lib/src/main.cpp
@@ -12,14 +12,14 @@ int c;
 std::string filename = "";
 bool isDump = false;
 auto gadgetLoader = Frontend::R2::from;
+std::map<RegType::Reg, uint64_t> dests;
 
 void parseArgs(int argc, char **argv);
 
 int main(int argc, char **argv) {
     parseArgs(argc, argv);
     auto gadgets = gadgetLoader(filename).value();
-    auto rop = Solver::solveAvoidChars({{RegType::rdi, 0x41414141},
-            {RegType::rsi, 0x42424242}}, gadgets, 0x2122232425262728, {});
+    auto rop = Solver::solveAvoidChars(dests, gadgets, 0x2122232425262728, {});
     if(!rop.has_value()) {
         std::cerr << "Error" << std::endl;
         exit(1);
@@ -32,14 +32,28 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+uint64_t fromStr(char *s) {
+    int base = 10;
+    if(!strncmp(s, "0x", 2)) {
+        base = 16;
+    }
+    return std::stoul(s, nullptr, base);
+}
+
 void parseArgs(int argc, char **argv) {
     static struct option ops[] = {
         {"rax", 1, NULL, 'A'},
+        {"eax", 1, NULL, 'A'},
         {"rbx", 1, NULL, 'B'},
+        {"ebx", 1, NULL, 'B'},
         {"rcx", 1, NULL, 'C'},
+        {"ecx", 1, NULL, 'C'},
         {"rdx", 1, NULL, 'D'},
+        {"edx", 1, NULL, 'D'},
         {"rdi", 1, NULL, 'E'},
+        {"edi", 1, NULL, 'E'},
         {"rsi", 1, NULL, 'F'},
+        {"esi", 1, NULL, 'F'},
         {"r8", 1, NULL, 'G'},
         {"r9", 1, NULL, 'H'},
         {"r10", 1, NULL, 'I'},
@@ -50,17 +64,20 @@ void parseArgs(int argc, char **argv) {
         {"r15", 1, NULL, 'N'}
     };
     int ops_index;
-    while((c = getopt_long(argc, argv, "a:b:df:", ops, &ops_index)) != EOF) {
+    while((c = getopt_long(argc, argv, "a:b:df:", ops, &ops_index) != EOF)) {
+        if(c == 'a') {
+            if(!strcmp("x86", optarg)) {
+                Arch::arch = Arch::AMD64;
+            } else if(!strcmp("amd64", optarg)) {
+                Arch::arch = Arch::X86;
+            } else {
+                Arch::arch = Arch::AMD64;
+            }
+        }
+    }
+    optind = 0;
+    while((c = getopt_long(argc, argv, "b:df:", ops, &ops_index)) != EOF) {
         switch(c) {
-            case 'a':
-                if(!strcmp("x86", optarg)) {
-                    Arch::arch = Arch::AMD64;
-                } else if(!strcmp("amd64", optarg)) {
-                    Arch::arch = Arch::X86;
-                } else {
-                    Arch::arch = Arch::AMD64;
-                }
-                break;
             case 'b':
                 if(!strcmp("r2", optarg)) {
                     gadgetLoader = Frontend::R2::from;
@@ -74,47 +91,87 @@ void parseArgs(int argc, char **argv) {
             case 'f':
                 filename = std::string(optarg);
                 break;
-            case 'A'://rax
-                //TODO
+            case 'A'://rax/eax
+                if(Arch::arch == Arch::X86) {
+                    dests[RegType::eax] = fromStr(optarg);
+                } else {
+                    dests[RegType::rax] = fromStr(optarg);
+                }
                 break;
-            case 'B'://rbx
-                //TODO
+            case 'B'://rbx/ebx
+                if(Arch::arch == Arch::X86) {
+                    dests[RegType::ebx] = fromStr(optarg);
+                } else {
+                    dests[RegType::rbx] = fromStr(optarg);
+                }
                 break;
-            case 'C'://rcx
-                //TODO
+            case 'C'://rcx/ecx
+                if(Arch::arch == Arch::X86) {
+                    dests[RegType::ecx] = fromStr(optarg);
+                } else {
+                    dests[RegType::rcx] = fromStr(optarg);
+                }
                 break;
-            case 'D'://rdx
-                //TODO
+            case 'D'://rdx/edx
+                if(Arch::arch == Arch::X86) {
+                    dests[RegType::edx] = fromStr(optarg);
+                } else {
+                    dests[RegType::rdx] = fromStr(optarg);
+                }
                 break;
-            case 'E'://rdi
-                //TODO
+            case 'E'://rdi/edi
+                if(Arch::arch == Arch::X86) {
+                    dests[RegType::edi] = fromStr(optarg);
+                } else {
+                    dests[RegType::rdi] = fromStr(optarg);
+                }
                 break;
-            case 'F'://rsi
-                //TODO
+            case 'F'://rsi/esi
+                if(Arch::arch == Arch::X86) {
+                    dests[RegType::esi] = fromStr(optarg);
+                } else {
+                    dests[RegType::rsi] = fromStr(optarg);
+                }
                 break;
             case 'G'://r8
-                //TODO
+                if(Arch::arch == Arch::AMD64) {
+                    dests[RegType::r8] = fromStr(optarg);
+                }
                 break;
             case 'H'://r9
-                //TODO
+                if(Arch::arch == Arch::AMD64) {
+                    dests[RegType::r9] = fromStr(optarg);
+                }
                 break;
             case 'I'://r10
-                //TODO
+                if(Arch::arch == Arch::AMD64) {
+                    dests[RegType::r10] = fromStr(optarg);
+                }
                 break;
             case 'J'://r11
-                //TODO
+                if(Arch::arch == Arch::AMD64) {
+                    dests[RegType::r11] = fromStr(optarg);
+                }
                 break;
             case 'K'://r12
-                //TODO
+                if(Arch::arch == Arch::AMD64) {
+                    dests[RegType::r12] = fromStr(optarg);
+                }
                 break;
             case 'L'://r13
-                //TODO
+                if(Arch::arch == Arch::AMD64) {
+                    dests[RegType::r13] = fromStr(optarg);
+                }
                 break;
             case 'M'://r14
-                //TODO
+                if(Arch::arch == Arch::AMD64) {
+                    dests[RegType::r14] = fromStr(optarg);
+                }
                 break;
             case 'N'://r15
-                //TODO
+                if(Arch::arch == Arch::AMD64) {
+                    dests[RegType::r15] = fromStr(optarg);
+                }
                 break;
         }
     }

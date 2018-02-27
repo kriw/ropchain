a69cc51a215e5d4b4ba182d27fb1d7baf18a487a
kriw <kotarou777775@gmail.com>
2018-02-27 10:39:40 +0900

    Fix .travis.yml

--- ropchain/ffi/src/common/frontend/rp++/rp_script.sh
+++ ropchain/ffi/src/common/frontend/rp++/rp_script.sh
@@ -1,6 +1,5 @@
 tmpFile='/tmp/tmpGadget.txt'
-rp++ --file=$1 --rop=2 > $tmpFile
-# rp++ --file=$1 --rop=6 > $tmpFile
+rp++ --file=$1 --rop=6 > $tmpFile
 sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" $tmpFile
 grep '0x' $tmpFile |
 grep -v 'push' | #avoid using push insn

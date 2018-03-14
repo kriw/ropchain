4906da720a8983ea5ee9972af400e37db4ff6e7c
kriw <kotarou777775@gmail.com>
2018-03-14 17:18:11 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ class CustomInstallCommand(install):
  
 setup(
         name             = 'ropchain',
-        version          = '0.1.9.1',
+        version          = '0.1.9.2',
         description      = 'ROPChain generator',
         license          = 'GPL3.0',
         author           = 'kriw',
